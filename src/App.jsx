aaa638349bbc590cb9c32867821bee2741bdf33b
thoughtlessnerd <abhay.csgo001@gmail.com>
2025-03-31 18:56:25 +0530

    navbar base

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <>
-      <h1 className="text-4xl w-full text-center my-4">HASH DEFINE</h1>
+      <nav className="navbar flex justify-between py-10 px-12">
+        <div className="logo items-center flex">Hash Define</div>
+        <ul className="links flex gap-12 font-bold text-lg">
+          <li>Events</li>
+          <li>About</li>
+          <li>Our Team</li>
+        </ul>
+        <button className="bg-slate-200/20 border-2 rounded-[50px] px-6 text-lg font-light">Contact Us</button>
+      </nav>
     </>
   );
 }
