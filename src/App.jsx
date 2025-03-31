d6e67a2236cbab635e0863dd1a6e4f17e609e870
thoughtlessnerd <abhay.csgo001@gmail.com>
2025-03-31 19:12:58 +0530

    sections made

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <>
       <Navbar />
+      <section className="hero min-h-screen bg-red-300"></section>
+      <section className="bento min-h-screen bg-green-300"></section>
+      <section className="placements min-h-screen bg-amber-300"></section>
+      <section className="contact min-h-screen bg-blue-300"></section>
       <Footer />
     </>
   );
